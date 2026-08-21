4ee12f0bf804354414f7bac4309a21f6c78c660d
Blessing Emmanuel <blessing.emmanuel@hotmail.com>
2026-08-21 15:50:27 +0100

    sect(4): add candidate-facing evaluate_system_state stub

---
+++ src/logic.c
@@ -0,0 +1,10 @@
+#include "logic.h"
+
+SystemOutputs evaluate_system_state(BmsData bms, BatteryTempData batt_temp,
+                                     InverterData inverter, PvData pv,
+                                     GridData grid, RelayState relay,
+                                     DoorSwitchData door) {
+    SystemOutputs out = {0};
+    // TODO: implement
+    return out;
+}
